7fafaa49fb7e0c0d2f3566894d80764017cb03f2
NaomiP <nperenboom@hotmail.com>
2024-12-01 20:52:53 +0100

    now in a do loop & trimming at start and end if empty space

--- AnagramApp/Program.cs
+++ AnagramApp/Program.cs
@@ -1,21 +1,20 @@
-﻿Console.Write("Hello, World! I am a anagram tester. Please give me two messages and I will tell you if it indeed is an anagram! Message 1:");
-string messageOne = Console.ReadLine().ToLower();
-Console.Write("Message 2:");
-string messageTwo = Console.ReadLine().ToLower();
-
-//Console.WriteLine(SortString(messageOne, messageTwo));
-
-if (messageOne.Length == messageTwo.Length)
+﻿string goAgain = string.Empty;
+do
 {
-    Console.WriteLine("same length");
-}
-string ns1 = String.Concat(messageOne.OrderBy(c => c));
-string ns2 = String.Concat(messageTwo.OrderBy(c => c));
+	Console.Write("I am a anagram tester. Please give me two strings and I will tell you if it indeed is an anagram! Message 1:");
+	string messageOne = Console.ReadLine().ToLower();
+	Console.Write("Message 2:");
+	string messageTwo = Console.ReadLine().ToLower();
 
-if (ns1 == ns2)
-{
-    Console.WriteLine("same values");
-}
+	string ns1 = String.Concat(messageOne.OrderBy(c => c)).TrimStart().TrimEnd();
+	string ns2 = String.Concat(messageTwo.OrderBy(c => c)).TrimStart().TrimEnd();
+
+	if (/*messageOne.Length == messageTwo.Length && */ ns1 == ns2)
+	{
+		Console.WriteLine("both length and values are the same, it is an anagram. Want to go again? Y/N?");
+        goAgain = Console.ReadLine().ToUpper();
+    }
+} while (goAgain == "Y");
 
 
 //Same letters, same length, different arrangement
