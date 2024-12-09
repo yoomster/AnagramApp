bd48839266ddc85e39e1cff018749b06e3be3342
yoomster <nperenboom@hotmail.com>
2024-12-09 15:15:35 +0100

    version 2.0

--- AnagramApp/Program.cs
+++ AnagramApp/Program.cs
@@ -1,21 +1,4 @@
-﻿//string goAgain = string.Empty;
-//do
-//{
-//	Console.Write("I am a anagram tester. Give two strings and I'll tell you if it's an anagram! Message 1:");
-//	string messageOne = Console.ReadLine().ToLower();
-//	Console.Write("Message 2:");
-//	string messageTwo = Console.ReadLine().ToLower();
-//	string ns1 = String.Concat(messageOne.OrderBy(c => c)).TrimStart().TrimEnd();
-//	string ns2 = String.Concat(messageTwo.OrderBy(c => c)).TrimStart().TrimEnd();
-
-//	if (/*messageOne.Length == messageTwo.Length && */ ns1 == ns2)
-//	{
-//		Console.WriteLine("both length and values are the same, it is an anagram. Want to go again? Y/N?");
-//        goAgain = Console.ReadLine().ToUpper();
-//    }
-//} while (goAgain == "Y");
-
-while (true)
+﻿while (true)
 {
 	Console.Write("I am an anagram tester. \nGive two strings and I'll tell you if it's an anagram! \nMessage 1:");
 	string message1 = Console.ReadLine().ToLower();
